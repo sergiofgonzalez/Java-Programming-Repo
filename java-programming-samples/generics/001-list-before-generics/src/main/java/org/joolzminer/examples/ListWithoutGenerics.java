ae358bd86330062fa8f9f2397333b5c11fad90ce
sergio.f.gonzalez <sergio.f.gonzalez@gmail.com>
2014-02-16 19:24:40 +0100

    Added example with Generic List to 001-list-before-generics.

--- java-programming-samples/generics/001-list-before-generics/src/main/java/org/joolzminer/examples/ListWithoutGenerics.java
+++ java-programming-samples/generics/001-list-before-generics/src/main/java/org/joolzminer/examples/ListWithoutGenerics.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class ListWithoutGenerics {
 	@SuppressWarnings({ "unchecked", "rawtypes", "unused" })
 	public static void main(String[] args) {
+		
+		// List before generics
 		List stringList = new ArrayList();
 		stringList.add("hello");
 		stringList.add("world");
@@ -13,5 +15,16 @@ public class ListWithoutGenerics {
 		String item = (String) stringList.get(1);
 		
 		stringList.add(new ListWithoutGenerics());
+		System.out.println(stringList);
+		
+		// Generic List
+		List<String> genericStringList = new ArrayList<>();
+		genericStringList.add("hello");
+		genericStringList.add("world");
+		
+		// Error: not applicable for those arguments
+		//genericStringList.add(new ListWithoutGenerics());
+		
+		System.out.println(genericStringList);
 	}
 }
