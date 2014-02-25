7faa4c5a095a4f9d650c4be365a29de2f0d130bf
sergio.f.gonzalez <sergio.f.gonzalez@gmail.com>
2014-02-25 21:01:29 +0100

    Added switch statement example to 002-statements.

---
+++ java-programming-samples/misc-basic-concepts/002-statements/src/test/java/org/joolzminer/examples/SwitchStatementTest.java
@@ -0,0 +1,29 @@
+package org.joolzminer.examples;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SwitchStatementTest {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SwitchStatementTest.class);
+	
+	@Test
+	public void testSwitchOnString() {
+		String option = "two";
+		switch (option) {
+			case "one":
+				LOGGER.info("one");
+				break;
+
+			case "two":
+				LOGGER.info("two");
+				break;
+				
+			case "three":
+				LOGGER.info("three");
+			default :
+				LOGGER.info("option is not one, two or three");
+				break;
+		}		
+	}	
+}
