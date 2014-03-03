ffdb6072f45c9b7ef83b65f31d5e67ddfd58e183
sergio.f.gonzalez <sergio.f.gonzalez@gmail.com>
2014-03-03 13:47:16 +0100

    Added CalendarTestRunner to 008-numbers-and-dates.

---
+++ java-programming-samples/misc-basic-concepts/008-numbers-and-dates/src/main/java/org/joolzminer/examples/CalendarTestRunner.java
@@ -0,0 +1,48 @@
+package org.joolzminer.examples;
+
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class CalendarTestRunner {
+
+	public static void main(String[] args) {
+		// Instantiating a calendar
+		Calendar calendar = Calendar.getInstance();
+		System.out.println(calendar);
+		
+		// Getting a Date instance representing the same date as the Calendar
+		Date date = calendar.getTime();
+		System.out.println(date);
+		
+		// Getting the components from a Calendar instance
+		int year = calendar.get(Calendar.YEAR);
+		int month = calendar.get(Calendar.MONTH);
+		int day = calendar.get(Calendar.DAY_OF_MONTH);
+		int calDate = calendar.get(Calendar.DATE);
+		int hour = calendar.get(Calendar.HOUR);
+		int minute = calendar.get(Calendar.MINUTE);
+		int second = calendar.get(Calendar.SECOND);
+		int millis = calendar.get(Calendar.MILLISECOND);
+		
+		System.out.println("year=" + year + ", " 
+						+ "month=" + month + ", "
+						+ "day=" + day + ", "
+						+ "date=" + calDate + ", "
+						+ "hour=" + hour + ", "
+						+ "minute=" + minute + ", "
+						+ "second=" + second + ", "
+						+ "milliseconds=" + millis);
+		
+		// Constructing a Calendar instance from a Date object
+		Date now = new Date();
+		Calendar nowCalendar = Calendar.getInstance();
+		nowCalendar.setTime(now);		
+		System.out.println(nowCalendar);
+		
+		// Adjust the calendar to change components:
+		calendar.set(Calendar.YEAR, 2015);
+		System.out.println(calendar);
+	}
+}
