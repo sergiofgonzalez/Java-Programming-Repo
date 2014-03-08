a8d50271c02754074f0cdd633a98981f9737f20c
sergio.f.gonzalez <sergio.f.gonzalez@gmail.com>
2014-03-08 22:37:09 +0100

    Add a JListTest to 001-basic-desktop-ui.

---
+++ java-programming-samples/desktop-ui/001-basic-desktop-ui/src/main/java/org/joolzminer/examples/JListTest.java
@@ -0,0 +1,35 @@
+package org.joolzminer.examples;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.SwingUtilities;
+
+public class JListTest {
+	private static void constructGUI() {
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		JFrame frame = new JFrame("JList test");
+		frame.setLayout(new FlowLayout());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		String[] selections = {"green", "red", "orange", "dark blue"};
+		JList<String> list = new JList<>(selections);
+		list.setSelectedIndex(1);
+		
+		System.out.println(list.getSelectedValue());
+		frame.add(list);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				constructGUI();
+			}
+		});
+	}
+}
