526e5aff9142a20287a2a8713a0a538d653be852
sergio.f.gonzalez <sergio.f.gonzalez@gmail.com>
2014-03-13 17:01:39 +0100

    Added RunnableDemo to 001-basic-threading.

---
+++ java-programming-samples/concurrency/001-basic-threading/src/main/java/org/joolzminer/examples/RunnableDemo.java
@@ -0,0 +1,26 @@
+package org.joolzminer.examples;
+
+public class RunnableDemo implements Runnable {
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// swallow exception
+			}
+		}		
+	}
+
+	public static void main(String[] args) {
+		RunnableDemo runnableDemo = new RunnableDemo();
+		Thread thread = new Thread(runnableDemo);
+		
+		System.out.print("*** starting RunnableDemo Thread: ");
+		thread.start();
+		System.out.println("done");
+	}
+	
+}
