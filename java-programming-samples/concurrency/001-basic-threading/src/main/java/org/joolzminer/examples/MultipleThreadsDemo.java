e650bfb8ed7dff2f3c6e8a1ac5e991f6159409c7
sergio.f.gonzalez <sergio.f.gonzalez@gmail.com>
2014-03-13 17:18:15 +0100

    Added MultipleThreadsDemo to 001-basic-threading.

---
+++ java-programming-samples/concurrency/001-basic-threading/src/main/java/org/joolzminer/examples/MultipleThreadsDemo.java
@@ -0,0 +1,83 @@
+package org.joolzminer.examples;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+public class MultipleThreadsDemo extends JFrame {
+	private JLabel countUpLabel = new JLabel("Count up");
+	private JLabel countDownLabel = new JLabel("Cound down");
+	
+	class CountUpThread extends Thread {
+
+		@Override
+		public void run() {
+			int count = 0;
+			while (true) {
+				try {
+					sleep(100);
+				} catch (InterruptedException e) {
+					// Swallow exception
+				}
+				if (count == 1000) {
+					count = 0;
+				}
+				countUpLabel.setText(Integer.toString(count++));
+			}
+		}		
+	}
+	
+	class CountDownThread extends Thread {
+
+		@Override
+		public void run() {
+			int count = 1000;
+			while (true) {
+				try {
+					sleep(100);
+				} catch (InterruptedException e) {
+					// Swallow exception
+				}
+				if (count == 0) {
+					count = 1000;
+				}
+				countDownLabel.setText(Integer.toString(count--));
+			}
+		}		
+	}
+	
+	public MultipleThreadsDemo(String title) {
+		super(title);
+		init();
+	}
+	
+	private void init() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new FlowLayout());
+		add(countUpLabel);
+		add(countDownLabel);
+		
+		pack();
+		setVisible(true);
+		
+		(new CountDownThread()).start();
+		(new CountUpThread()).start();
+	}
+	
+	private static void constructGUI() {
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		new MultipleThreadsDemo("Multiple Threads Demo");
+	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				constructGUI();
+			}
+		});
+	}
+}
